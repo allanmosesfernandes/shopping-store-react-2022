71a0dcc8a922cf0d217c359073165972ae912d1a
allanmosesfernandes <fernzallan14@gmail.com>
2022-12-05 15:44:28 +0000

    scaffold

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import './App.css';
+import './assets/categories.styles.scss';
+import React from 'react'
+import CategoryItem from './components/category-item.component';
+const App = () => {
+
+  const categories = [
+    {
+      "id": 1,
+      "title": "hats",
+      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
+    },
+    {
+      "id": 2,
+      "title": "jackets",
+      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
+    },
+    {
+      "id": 3,
+      "title": "sneakers",
+      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
+    },
+    {
+      "id": 4,
+      "title": "womens",
+      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
+    },
+    {
+      "id": 5,
+      "title": "mens",
+      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
+    }
+  ]
+  
+
+
+  return (
+    <div className='categories-container'>
+      {categories.map((category) => <CategoryItem category={category} />)}
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/category-item.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const CategoryItem = ({category}) => {
+  return (
+    <div className='category-container'>
+      <div className='background-image' style={{backgroundImage: `url(${category.imageUrl})`}} />
+        <div className='category-body-container'>
+          <h1>{category.title}</h1>
+          <p>Shop now</p>
+        </div>
+      </div>
+  )
+}
+
+export default CategoryItem
